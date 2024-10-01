5e593870b6fc9b61148360e956b4cabe68888ba7
4u11 <mifmaul06@gmail.com>
2024-10-01 10:31:30 +0700

    Add files via upload

---
+++ odd or even/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        
+        string input = Console.ReadLine();
+
+        
+        if (int.TryParse(input, out int number))
+        {
+            
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is odd.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid integer.");
+        }
+    }
+}
