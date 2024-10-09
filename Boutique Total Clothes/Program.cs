936d717fc21057e83da993bef19b4064807afb21
4u11 <mifmaul06@gmail.com>
2024-10-09 11:38:39 +0700

    Add files via upload

---
+++ Boutique Total Clothes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=== Welcome to the Boutique's clothes store ===");
+            Console.WriteLine(" ");
+            Console.WriteLine("Input the T-shirts: ");
+            int tShirt = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Input the Shirts: ");
+            int shirt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the Sweaters: ");
+            int sweater = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the Pants: ");
+            int pant = Convert.ToInt32(Console.ReadLine());
+
+            int Total = tShirt +  shirt + sweater + pant;
+            Console.WriteLine("Total: " + Total);
+
+            
+
+        }
+    }
