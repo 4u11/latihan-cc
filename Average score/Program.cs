f3f799a624d80304439d4859afa9060935d75ff0
4u11 <mifmaul06@gmail.com>
2024-10-09 11:30:46 +0700

    Add files via upload

---
+++ Average score/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=== Welcome to calculate the average score!!! ===");
+            Console.WriteLine(" ");
+            Console.Write("Enter the number of assignment scores: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the number of Quiz: ");
+            double q = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the number of MidTerm: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the number of Final: ");
+            double f = Convert.ToDouble(Console.ReadLine());
+
+            double average =  (n + q + r + f) / 4;
+
+            Console.WriteLine("Your average score is: " + average);
+            
+            
+
+        }
+    }
