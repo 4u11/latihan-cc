670732e02fabec95e71d3906ee8f1e11cfcbf2b5
Fubuki <mifmaul06@gmail.com>
2024-10-10 08:30:48 +0700

    Add files via upload

---
+++ The power of__/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void CalculateExponent(long number, long exponent) 
+    {
+        Console.WriteLine($"Calculating the power of {number}...");
+        for (long power = 0; power <= exponent; power++)
+            Console.WriteLine($"{number}^{power} = {(long)Math.Pow(number, power):N0} (0x{(long)Math.Pow(number, power):X})");
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Input the number:");
+        long number = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine("Input the exponent:");
+        long exponent = Convert.ToInt64(Console.ReadLine());
+        CalculateExponent(number, exponent);
+    }
+}
