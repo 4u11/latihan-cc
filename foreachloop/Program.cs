5cfedde4dea02d4656ba71643d51280e7a4d4872
Fubuki <mifmaul06@gmail.com>
2024-10-16 13:51:30 +0700

    Add files via upload

---
+++ foreachloop/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int Keqing = 0;
+int oddCount = 0;
+
+foreach (int i in numbers)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine((i + 1) + " is even");
+        Keqing++;
+    }
+    else
+    {
+        Console.WriteLine((i + 1) + " is odd");
+        oddCount++;
+    }
+}
+
+Console.WriteLine("Number of even numbers: " + Keqing);
+Console.WriteLine("Number of odd numbers: " + oddCount);
